d3ce143e1cbf564dd831d8c68bc0d34ad19e2a93
Rigon <rigon.osmanaj@beriflapp.com>
2024-12-18 10:45:45 +0100

    Jumbotron Section - Created jumbotron component - Added the new component in main section of app-route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, JumbotronComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
