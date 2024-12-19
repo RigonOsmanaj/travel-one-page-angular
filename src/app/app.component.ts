8fbe327a960121ccbc125a3e7413159a80e7f90a
Rigon <rigon.osmanaj@beriflapp.com>
2024-12-19 15:24:40 +0100

    Finish Home Page - Fixed destinations component - Created contact us component using reactiv angular form - Created footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
+
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
-import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { DestinationsComponent } from './components/destinations/destinations.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    HeaderComponent,
-    JumbotronComponent,
-    AboutUsComponent,
-    DestinationsComponent,
-  ],
+  imports: [RouterModule, HeaderComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
