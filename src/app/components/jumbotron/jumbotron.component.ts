faa41e5df7f2791f7da42c8a8453ec01659a4284
Rigon <rigon.osmanaj@beriflapp.com>
2024-12-24 15:21:49 +0100

    Jumbotron button ridirection fix

--- src/app/components/jumbotron/jumbotron.component.ts
+++ src/app/components/jumbotron/jumbotron.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-jumbotron',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './jumbotron.component.html',
-  styleUrl: './jumbotron.component.scss'
+  styleUrl: './jumbotron.component.scss',
 })
-export class JumbotronComponent {
-
-}
+export class JumbotronComponent {}
